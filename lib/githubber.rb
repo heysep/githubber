aea08e98bb205693da4dfa015f0c4dd616d7320d
heysep <sdvakili@gmail.com>
2015-10-13 12:28:36 -0400

    Assign var to class instance with token

--- lib/githubber.rb
+++ lib/githubber.rb
@@ -1,5 +1,11 @@
+require "httparty"
+require "pry"
+
 require "githubber/version"
+require "githubber/issues"
 
 module Githubber
   # Your code goes here...
+  api = Issues.new("861af2db0c4cfc39ce9b835206a1511d595cb8b9")
+  binding.pry
 end
